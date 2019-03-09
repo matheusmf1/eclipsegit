9ffe8b8e00c81ad9b7f13ab7fa5e3e9aa65cc4f8
Matheus Mandotti <16.00177-0@maua.br>
2019-03-09 12:36:23 -0300

    Update Hellogit.java
    Add a Hello World

--- Eclipsegit/src/br/maua/git/Hellogit.java
+++ Eclipsegit/src/br/maua/git/Hellogit.java
@@ -5,6 +5,7 @@ public class Hellogit {
 	public static void main(String[] args) {
 		System.out.println("Hello git...");
 		System.out.println("Hello git 2...");
-		System.out.println("Hello git 3 from Eclipse...");		
+		System.out.println("Hello git 3 from Eclipse...");	
+		System.out.println("Hello World");	
 	}
 }
