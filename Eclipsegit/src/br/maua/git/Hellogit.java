46b7bc72109058ead896ba83b6405d27b6771384
16.00177-0 <16.00177-0@E2-A05.maua.br>
2019-03-09 12:35:39 -0300

    Add hello from IDE

--- Eclipsegit/src/br/maua/git/Hellogit.java
+++ Eclipsegit/src/br/maua/git/Hellogit.java
@@ -5,6 +5,6 @@ public class Hellogit {
 	public static void main(String[] args) {
 		System.out.println("Hello git...");
 		System.out.println("Hello git 2...");
-		
+		System.out.println("Hello git 3 from Eclipse...");		
 	}
 }
